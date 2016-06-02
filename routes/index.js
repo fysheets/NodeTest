b8779662f0c20adebd45613b789eb78dd20f14fb
Sheets, Farhanah Y <Farhanah.Y.Sheets@espn.com>
2016-06-01 20:56:06 -0400

    Additional logic for Updating and Deleting users from Mongo Very basic UI for performing these actions Will return user to Library site after updates

--- routes/index.js
+++ routes/index.js
@@ -69,7 +69,94 @@ router.post('/adduser', function(req, res) {
     });
 });
 
-/* GET users from mongo */
+/* GET Edit User page. */
+router.get('/editUser?:user', function(req, res) {
+	var db = req.db;
+    var collection = db.get('usercollection');
+    console.log(req.query.user);
+    collection.find({'username': req.query.user},{},function(e,docs){
+        console.log(docs);
+        console.log(docs[0].username);
+        res.render('editUser', {
+        	title: 'Edit User', 
+            "user" : docs[0]
+        });
+    });
+});
+
+/* Update User */
+router.post('/updateuser', function(req, res) {
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values
+    var userName = req.body.username;
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var hometown = req.body.hometown;
+    var favorite = req.body.favorite;
+    var image = req.body.image;
+    var birthday = new Date(req.body.birthday).getTime();
+    var bookClub = req.body.bookClub;
+    var books = req.body.books;
+console.log(image);
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.update({
+        "username" : userName
+    }, {
+    	"username" : userName,
+        "firstName" : firstName,
+        "lastName" : lastName,
+        "hometown" : hometown,
+        "favorite" : favorite,
+        "image" : image,
+        "birthday" : birthday,
+        "bookClub" : bookClub,
+        "books" : books
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect("http://mylibrary.test.com:8000/MyLibrary/#/readers");
+        }
+    });
+});
+
+/* DELETE User */
+router.post('/removeuser', function(req, res) {
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values
+    var userName = req.body.username;
+console.log("delete");
+console.log(userName);
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.remove({
+        "username" : userName
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect("http://mylibrary.test.com:8000/MyLibrary/#/readers");
+        }
+    });
+});
+
+
+/* GET books from mongo */
 router.get('/testBooks', function(req, res) {
     var db = req.db;
     var collection = db.get('bookcollection');
